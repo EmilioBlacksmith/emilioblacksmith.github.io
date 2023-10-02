caec7804bd718932f1650ed212d7e0208315a460
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-01 18:46:37 -0600

    initial project boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-function App() {
+export default function App() {
   return (
     <>
       <div>
         <h2>Howdy, I'm Emilio Blacksmith</h2>
       </div>
     </>
-  )
+  );
 }
-
-export default App
