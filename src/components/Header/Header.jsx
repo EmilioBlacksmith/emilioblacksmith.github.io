fd4462a87e85ae83baa17dbc6bd44ae0b09aa1c5
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-06 19:56:17 -0600

    darkmode feature done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react'
 import './Header.css'
 
 export function Header() {
+    const [theme, setTheme] = useState(() =>{
+        let selectedTheme = localStorage.getItem("selectedTheme");
+
+        if(selectedTheme !== null){
+            return selectedTheme;
+        }
+
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+            return 'dark';
+        }
+
+        return 'light';
+    });
+
+    const handleDarkMode = (e) => {
+        if(e.target.checked){
+            setTheme('dark');
+            localStorage.setItem("selectedTheme", 'dark')
+        }
+        else{
+            setTheme('light');
+            localStorage.setItem("selectedTheme", 'light')
+        }
+    }
+
+    useEffect(() =>{
+        if(theme === 'dark')
+            document.querySelector('html').setAttribute("data-theme", 'dark');
+        else
+            document.querySelector('html').setAttribute("data-theme", 'light');
+    }, [theme]) 
+
     return (
         <>
             <div className="Header">
@@ -9,6 +42,8 @@ export function Header() {
                     className='darkModeToggle'
                     type='checkbox'
                     id='darkmode-toggle'
+                    onChange={handleDarkMode}
+                    defaultChecked={theme === 'dark'}
                 ></input>
             </div>
         </>
