f269d2dab12e53a7d195e89a1ad82c59780bbb06
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-27 00:10:16 -0600

    update footer links

--- src/containers/content/More/More.jsx
+++ src/containers/content/More/More.jsx
@@ -1,7 +1,26 @@
+import './More.css'
+
 export function More(){
     return(
         <section className="hidden" id="More-Section">
-            <h1>/* I DO OTHER THINGS 💀*/</h1>
+            <div className='more_data'>
+                <div className='SectionName'>
+                    <h1>/* I DO OTHER THINGS 💀*/</h1>
+                </div>
+                <div className='More'>
+                    <div>
+                        <h3>Youtube</h3>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>post</h3>
+                        </div>
+                        <div>
+                            <h3>post</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
