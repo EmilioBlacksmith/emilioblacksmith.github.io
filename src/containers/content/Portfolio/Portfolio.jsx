a2ef28fc49846dfad23665560ba714f3ab59dfb2
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-26 21:54:45 -0600

    update animations to avoid future issues

--- src/containers/content/Portfolio/Portfolio.jsx
+++ src/containers/content/Portfolio/Portfolio.jsx
@@ -1,7 +1,24 @@
+import './Portfolio.css'
+
 export function Portfolio(){
     return(
         <section className="hidden">
-            <h1>/* MY PORTFOLIO 📑*/</h1>
+            <div className='portfolio_data'>
+                <div className="SectionName">
+                    <h1>/* SOME OF MY RECENT WORK 📑*/</h1>
+                </div>
+                <div className="Projects">
+                    <div className="PortfolioProject">
+                        <h3>2023</h3>
+                        <h1>BIOLINKS</h1>
+                        <p>
+                            A minimalistic responsive website to showcase main social media links or project links, to share to others online.
+                            It is bases in Linktree and Linkin.bio.
+                        </p>
+                        <img src=""/>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
