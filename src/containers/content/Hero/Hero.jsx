9d8703ef706bac1029f8677b4ba28109df290353
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-10-28 19:26:32 -0600

    other section done

--- src/containers/content/Hero/Hero.jsx
+++ src/containers/content/Hero/Hero.jsx
@@ -14,7 +14,7 @@ export function Hero(){
                         <h1>HOWDY! 🤠</h1>
                         <p>I make Development fun to watch (Sometimes...)</p>
                         <div className='hero_cta_buttons'>
-                            <a>My Portfolio</a>
+                            <a href='#Portfolio-Section'>My Portfolio</a>
                             <a>Resume</a>
                         </div>
                     </div>
@@ -22,7 +22,7 @@ export function Hero(){
                 <div className='hero_aboutme'>
                     <div className='hero_whoami'>
                         <h2>WHO AM I?</h2>
-                        <p>I’m Emilio Herrera (also known as Emilio Blacksmith in my projects and the internet). I'm a Software Developer from México, I studied Interactive Design (UX / UI), with a focus on Development.</p>
+                        <p>I'm Emilio Herrera (also known as Emilio Blacksmith in my projects and the internet). I'm a Software Developer from México, I studied Interactive Design (UX / UI), with a focus on Development.</p>
                         <p>I make youtube videos about development, from game development to application development. I started making apps and videogames since 2017, my passion is to bring new and interesting experiences to my users. </p>
                     </div>
                     <div className='hero_tech'>
