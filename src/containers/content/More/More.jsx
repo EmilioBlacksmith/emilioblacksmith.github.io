2ea6ff55ee13c957d9124933273cc91606235fe5
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2023-11-02 21:12:18 -0600

    hotfix: youtube videos links update

--- src/containers/content/More/More.jsx
+++ src/containers/content/More/More.jsx
@@ -12,10 +12,10 @@ export function More(){
                         <h1>I Do Youtube 🎥</h1>
                         <p>I do youtube videos since 2020 about development, trying to make it as entertaining as possible. (most of the time)</p>
                         <div className='video_group'>
-                            <a href="https://youtu.be/Qnbxfw_oAZo?si=A2svs_74KfwsBntv"><img src="/assets/Images/youtube/youtube_video1.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
-                            <a href="https://youtu.be/Qnbxfw_oAZo?si=A2svs_74KfwsBntv"><img src="/assets/Images/youtube/youtube_video2.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
-                            <a href="https://youtu.be/Qnbxfw_oAZo?si=A2svs_74KfwsBntv"><img src="/assets/Images/youtube/youtube_video3.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
-                            <a href="https://youtu.be/Qnbxfw_oAZo?si=A2svs_74KfwsBntv"><img src="/assets/Images/youtube/youtube_video4.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
+                            <a href="https://youtu.be/Qnbxfw_oAZo?si=A2svs_74KfwsBntv" target='_blank'><img src="/assets/Images/youtube/youtube_video1.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
+                            <a href="https://youtu.be/h3VG6NCL3h8?si=IIeQUfEtShb1OP_4" target='_blank'><img src="/assets/Images/youtube/youtube_video2.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
+                            <a href="https://youtu.be/Ti1_qnA5-LY?si=vtEi4LTfAmQD6Qw7" target='_blank'><img src="/assets/Images/youtube/youtube_video3.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
+                            <a href="https://youtu.be/QO2iFnoGquk?si=8SFxLu5nDkqxEqSQ" target='_blank'><img src="/assets/Images/youtube/youtube_video4.webp" alt="thumbnail of video about Making FROGGER but with Ragdolls." className='video'/></a>
                         </div>
                         <a href="https://www.youtube.com/channel/UC1ZeE2i5QwVwhWOV-HveneQ" target='_blank'>Learn More...</a>
                     </div>
