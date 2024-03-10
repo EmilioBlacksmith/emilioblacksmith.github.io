87bbdf089ad77a41d3573b8bad986436ad75d593
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2024-03-10 13:17:33 -0600

    add prettier && initial folder convention setup

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-app-main w-screen h-screen text-white font-sans">
+      <div className="text-2xl">Hello World!</div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "app-main": "hsl(221, 12%, 32%)",
+      "app-secondary": "hsl(224, 12%, 19%)",
+      "app-secondary-dark": "hsl(220, 11%, 11%)",
+      white: "#FFF",
+      black: "#000",
+    },
+    fontFamily: {
+      sans: ["Hack"],
+    },
+    extend: {},
+  },
+  plugins: [],
 };
