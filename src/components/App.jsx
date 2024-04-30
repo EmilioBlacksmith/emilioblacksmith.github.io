783d93b123b9d0d759c5eebf0c7a360747e4ce62
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2024-04-29 20:42:54 -0600

    initial progress for about section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./Header";
+import About from "./About";
+import Hero from "./Hero";
 
 export default function App() {
   return (
-    <main className="flex h-screen w-screen justify-center bg-app-main font-sans text-white">
-      <section className="w-5/6">
+    <main className="flex h-screen w-screen flex-col items-center overflow-x-hidden bg-app-main font-sans text-white">
+      <header className="sticky top-0 w-5/6 bg-app-main">
         <Header />
+      </header>
+      <section className="w-5/6">
+        <Hero />
       </section>
     </main>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hero() {
+  return <section className="h-screen w-full bg-app-secondary">This</section>;
+}
