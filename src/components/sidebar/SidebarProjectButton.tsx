b7cfafca093267b27ecdf641a2df1f716151b321
emilioblacksmith <emilioblacksmithlush@gmail.com>
2024-06-18 09:40:22 -0600

    add about section datebar

--- src/components/sidebar/SidebarProjectButton.tsx
+++ src/components/sidebar/SidebarProjectButton.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface buttonData {
     title: string;
     year: string;
@@ -6,13 +8,13 @@ interface buttonData {
 
 function SidebarProjectButton({ title, year, direction }: buttonData) {
     return (
-        <a
-            href={direction}
+        <Link
+            to={direction}
             className="flex h-10 w-11/12 items-center justify-between rounded-full p-3 text-sm font-black transition-all ease-in-out hover:shadow-3xl"
         >
             <div>{title}</div>
             <div className="text-gray">{year}</div>
-        </a>
+        </Link>
     );
 }
 
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,6 +1,9 @@
+import DateBar from "../components/About/dateBar";
+
 function About() {
     return (
-        <div className="w-full p-4">
+        <div className="w-full p-8">
+            <DateBar />
             <h1>Hola soy Emilio</h1>
             <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
