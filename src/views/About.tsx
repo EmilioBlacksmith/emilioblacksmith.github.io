ba587f3b2c86a941eaea6564ab1c8539bb7d6fc7
emilioblacksmith <emilioblacksmithlush@gmail.com>
2024-06-18 09:44:42 -0600

    add description in about me

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -2,14 +2,16 @@ import DateBar from "../components/About/dateBar";
 
 function About() {
     return (
-        <div className="w-full p-8">
+        <div className="w-full p-10">
             <DateBar />
-            <h1>Hola soy Emilio</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Reiciendis culpa aut dicta aperiam temporibus consequuntur ex
-                facilis nostrum consectetur accusamus. Blanditiis deserunt rerum
-                deleniti et harum dicta laboriosam id? Nulla!
+            <p className="text-xl">
+                Emilio Herrera (Emilio Blacksmith) is a user-focused software
+                engineer with a degree in Interactive Design (UX/UI). He has
+                embarked on diverse projects, from video game development to
+                full-stack web applications. Previously, he created full-stack
+                projects for his YouTube channel and the Uncomfortably
+                Comfortable community. Currently, he is the founder and a
+                full-stack developer at Blacksmith Softworks.
             </p>
         </div>
     );
